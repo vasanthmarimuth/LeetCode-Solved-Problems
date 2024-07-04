3c7a5dd32eff8517412be585ac43dc6814526308
vasanth marimuthu <105814254+vasanthmarimuth@users.noreply.github.com>
2024-07-04 17:08:56 +0530

    Time: 2 ms (91.18%) | Memory: 42.4 MB (39.86%) - LeetSync

---
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int canBeTypedWords(String text, String brokenLetters) {
+        Set<Character> brokenSet = new HashSet<>();
+        for (char c : brokenLetters.toCharArray()) {
+            brokenSet.add(c);
+        }
+        
+        String[] words = text.split(" ");
+        int count = 0;
+        
+        for (String word : words) {
+            boolean canType = true;
+            for (char c : word.toCharArray()) {
+                if (brokenSet.contains(c)) {
+                    canType = false;
+                    break;
+                }
+            }
+            if (canType) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
