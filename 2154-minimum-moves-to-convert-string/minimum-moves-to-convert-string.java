e27ed3d03a736e3f181b3d7506b86ea241e3f92e
Vasanth Marimuthu <105814254+vasanthmarimuth@users.noreply.github.com>
2024-11-23 19:43:43 +0530

    Time: 1 ms (83.47%) | Memory: 41.6 MB (18.18%) - LeetSync

---
+++ 2154-minimum-moves-to-convert-string/minimum-moves-to-convert-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minimumMoves(String s) {
+        int i=0;
+        int step=0;
+        while(i<s.length()){
+            if(s.charAt(i)=='X'){
+                i+=3;
+                step++;
+            }else{
+                i++;
+            }
+        }
+        return step;
+    }
+}
